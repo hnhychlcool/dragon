63b54b0c7ef0aabdfabb2a7001729341b07ac37d
hzchl <hzchenhailong@corp.netease.com>
2017-08-16 15:37:12 +0800

    misc:add modules

---
+++ dragon-cate-system-common/src/main/java/com/dragon/cate/App.java
@@ -0,0 +1,13 @@
+package com.dragon.cate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
