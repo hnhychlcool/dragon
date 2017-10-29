eabfade35326c050a2c3d4dee29f560fa8bc4ff1
hzchl <hzchenhailong@corp.netease.com>
2017-10-29 22:27:04 +0800

    add properties config

--- dragon-cate-system-web/src/main/java/com/dragon/cate/web/application/Starter.java
+++ dragon-cate-system-web/src/main/java/com/dragon/cate/web/application/Starter.java
@@ -13,12 +13,14 @@ import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * 项目启动入口
  */
 @ComponentScan(basePackages = {"com.dragon.cate"})
+@PropertySource(encoding = "UTF-8",value = { "classpath:db.properties","classpath:nos.properties", "classpath:oms.properties", "classpath:redis.properties", "classpath:server.properties"})
 @EnableAutoConfiguration(exclude = {DataSourceTransactionManagerAutoConfiguration.class, SessionAutoConfiguration.class,
         DataSourceAutoConfiguration.class, FreeMarkerAutoConfiguration.class, SecurityAutoConfiguration.class})
 @SpringBootApplication
