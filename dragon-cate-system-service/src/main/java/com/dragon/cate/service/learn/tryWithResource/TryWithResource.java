eb54aceee6c782d1b569f5988c44e9941686f5bf
hnhychlcool <hnhychenhailong@163.com>
2018-07-05 15:30:09 +0800

    opt

--- dragon-cate-system-service/src/main/java/com/dragon/cate/service/learn/tryWithResource/TryWithResource.java
+++ dragon-cate-system-service/src/main/java/com/dragon/cate/service/learn/tryWithResource/TryWithResource.java
@@ -1,17 +1,18 @@
 package com.dragon.cate.service.learn.tryWithResource;
 
 /**
- *jdk7开始,基于ARM(Auto Resource Management)机制,try-with-resource语句可以对实现了AutoCloseable接口的资源进行自动释放,从而简化IO等资源文件的释放逻辑.
- 注意点:
- 1.如果try语句块正常,close语句异常,catch到的是close的异常
- 2.如果try语句块异常,close正常,catch到的是try语句块的异常
- 3.如果try语句块异常,close异常,catch到的是try块的异常,close的异常被忽略
+ * jdk7开始,基于ARM(Auto Resource Management)机制,try-with-resource语句可以对实现了AutoCloseable接口的资源进行自动释放,从而简化IO等资源文件的释放逻辑.
+ * 注意点:
+ * 1.如果try语句块正常,close语句异常,catch到的是close的异常
+ * 2.如果try语句块异常,close正常,catch到的是try语句块的异常
+ * 3.如果try语句块异常,close异常,catch到的是try块的异常,close的异常被忽略
  */
 public class TryWithResource {
 
     public static void main(String[] args) {
 
-        try(MyResource resource = new MyResource()){
+        // jdk9 开始,如果有一个对象是final或等效于final,那么可以在try()中直接使用,而不用再重新声明一个变量
+        try (MyResource resource = new MyResource()) {
             // try语句块正常,close异常,catch到close的异常
             System.out.println("try 语句块正常执行");
 
@@ -22,15 +23,15 @@ public class TryWithResource {
 
             // 找到被忽略的异常
             Throwable[] ts = e.getSuppressed();
-            for(Throwable t : ts) {
-                System.out.println("被忽略的异常: "+ t.getMessage());
+            for (Throwable t : ts) {
+                System.out.println("被忽略的异常: " + t.getMessage());
             }
         }
     }
 
 }
 
-class MyResource implements AutoCloseable{
+class MyResource implements AutoCloseable {
 
     @Override
     public void close() throws Exception {
